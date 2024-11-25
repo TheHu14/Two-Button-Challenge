d819ca4e82faf23642712047c566775cb9d6b5d1
Küng Hansueli <system-shock@web.de>
2024-11-25 21:59:00 +0100

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Küng Hansueli on 25.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Two-Button ChallengeTests/Two_Button_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Two_Button_ChallengeTests.swift
+//  Two-Button ChallengeTests
+//
+//  Created by Küng Hansueli on 25.11.2024.
+//
+
+import Testing
+@testable import Two_Button_Challenge
+
+struct Two_Button_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
